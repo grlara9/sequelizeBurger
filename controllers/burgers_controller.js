f135a36393411fcc1eff8e3fb9a2e728f6c0c099
Guillermo Lara <grlara9@gmail.com>
2019-12-15 15:09:56 -0800

    work on script.js for update

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,8 +2,11 @@ var express = require("express")
 var router = express.Router()
 var db = require("../models")
 
+router.get("/", (req,res) =>{
+    res.redirect("/burgers")
+})
 
-router.get("/", (req, res) => {
+router.get("/burgers", (req, res) => {
     db.Burgers.findAll({})
     .then(response => { console.log(response)
 
@@ -14,18 +17,18 @@ router.get("/", (req, res) => {
 })
 
 
-router.post("/create", (req, res)=>{
+router.post("/burgers/create", (req, res)=>{
     db.Burgers.create({
         burger_name: req.body.burger
         
     })
     .then(burger => {console.log(burger)
-    
+    res.redirect("/");
     })
     .catch(err=> console.log(err))
 })
 
-router.put("/burgers/update/:id", (req, res) =>{
+router.put("burgers/update/:id", (req, res) =>{
     db.Burgers.update({
         devoured: true
     },
@@ -34,7 +37,8 @@ router.put("/burgers/update/:id", (req, res) =>{
         id: req.params.id
     }
 }
-).then(burger => {
+).then(burger_put => {
+    console.log(burger_put)
     res.json("/")
 })
 })
